ae499fb4508095d590d581fb33e0a3f05a4fbc52
vinsis <sisodia.vinay@gmail.com>
2021-02-19 13:16:28 +0800

    added mvp

--- server.py
+++ server.py
@@ -1,10 +1,38 @@
+import os
+import glob
+from io import BytesIO
+from PIL import Image
 from flask import Flask, render_template, request
+from zoomie_stuff import main, template_files
+
+print('Found {} template files'.format(len(template_files)))
 
 app = Flask(__name__)
+
+def file_handle_to_pil_image(f):
+    image_bytes = BytesIO(f.read())
+    return Image.open(image_bytes).convert('RGB')
+
+DIRNAME = 'static/results'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    for fn in glob.glob(os.path.join(DIRNAME, '*')):
+        os.remove(fn)
+    assert len(glob.glob(os.path.join(DIRNAME, '*'))) == 0, 'Found {} file'.format(len(glob.glob(os.path.join(DIRNAME, '*'))))
     if request.method == 'POST':
-        return render_template('index.html')
+        f = request.files['content']
+        image = file_handle_to_pil_image(f).convert('RGB')
+        results = [main(image, Image.open(fn)) for fn in template_files]
+        filenames = []
+        for index, fn in enumerate(template_files):
+            print('processing', index)
+            result = main(image, Image.open(fn)).convert('RGB')
+            filename = os.path.join(DIRNAME, '{}.jpg'.format(index))
+            if os.path.exists(filename): os.remove(filename)
+            result.save(filename)
+            filenames.append(filename)
+        return render_template('index.html', filenames=filenames)
     else:
         return render_template('index.html')
 
---
+++ zoomie_stuff.py
@@ -0,0 +1,57 @@
+import numpy as np
+from PIL import Image
+from coremltools.models import MLModel
+import os
+import glob
+
+CWD = os.path.dirname(__file__)
+template_files = glob.glob(os.path.join(CWD, 'template_images', '*.png'))
+
+face_model = MLModel('face.mlmodel')
+
+def get_face_bboxes(img):
+    img = img.convert('RGB')
+    w, h = img.size
+    resized_img = img.resize((320, 240))
+    result = face_model.predict({'input_image': resized_img, 'iouThreshold': 0.1, 'confidenceThreshold': 0.5}, useCPUOnly=True)
+    coordinates = result['coordinates'][:]
+    # print("Face output", coordinates)
+    # cx, cy, w, h 
+    coordinates[:, 0] = coordinates[:, 0]*w
+    coordinates[:, 1] = coordinates[:, 1]*h
+    coordinates[:, 2] = coordinates[:, 2]*w
+    coordinates[:, 3] = coordinates[:, 3]*h
+    
+    bboxes = []
+    for cor in coordinates:
+        bboxes.append([cor[0]-cor[2]/2, cor[1]-cor[3]/2, cor[0]+cor[2]/2, cor[1]+cor[3]/2])
+    return bboxes
+
+to_int = lambda box: [int(i) for i in box]
+box_to_size = lambda box: (box[2]-box[0], box[3]-box[1])
+
+def image_to_face_scraps_or_boxes(image, return_boxes=False):
+    boxes = get_face_bboxes(image.convert('RGB'))
+    boxes = [to_int(box) for box in boxes]
+    if return_boxes:
+        return boxes
+    scraps = [image.crop(box) for box in boxes]
+    return scraps
+
+def put_faces_on_template(face_scraps, template_image):
+    boxes = image_to_face_scraps_or_boxes(template_image, return_boxes=True)
+    for scrap, box in zip(face_scraps, boxes):
+        size = box_to_size(box)
+        scrap = scrap.resize(size)
+        template_image.paste(scrap, box)
+    return template_image
+
+def main(zoom_image, template_image):
+    scraps = image_to_face_scraps_or_boxes(zoom_image)
+    return put_faces_on_template(scraps, template_image)
+
+if __name__ == '__main__':
+    image = Image.open('/Users/sisovina/Downloads/internal_zoom_call.png')
+    template_image = Image.open('/Users/sisovina/Downloads/template/image 22.png')
+    result = main(image, template_image).convert('RGB')
+    result.save('/Users/sisovina/Downloads/zoom_result.jpg')
