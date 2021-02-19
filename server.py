1e9351858dc69160037db3dd6ef605be5eec3458
bojne <bojne.john@gmail.com>
2021-02-19 14:41:48 +0800

    update html

--- server.py
+++ server.py
@@ -8,21 +8,29 @@ import string
 import random
 
 print('Found {} template files'.format(len(template_files)))
-rand_string = lambda: ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + '_', k=20))
+
+
+def rand_string(): return ''.join(random.choices(string.ascii_uppercase +
+                                                 string.ascii_lowercase + string.digits + '_', k=20))
+
 
 app = Flask(__name__)
+
 
 def file_handle_to_pil_image(f):
     image_bytes = BytesIO(f.read())
     return Image.open(image_bytes).convert('RGB')
 
+
 DIRNAME = 'static/results'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     for fn in glob.glob(os.path.join(DIRNAME, '*')):
         os.remove(fn)
-    assert len(glob.glob(os.path.join(DIRNAME, '*'))) == 0, 'Found {} file'.format(len(glob.glob(os.path.join(DIRNAME, '*'))))
+    assert len(glob.glob(os.path.join(DIRNAME, '*'))
+               ) == 0, 'Found {} file'.format(len(glob.glob(os.path.join(DIRNAME, '*'))))
     if request.method == 'POST':
         f = request.files['content']
         image = file_handle_to_pil_image(f).convert('RGB')
@@ -31,13 +39,16 @@ def index():
         for index, fn in enumerate(template_files):
             print('processing', index)
             result = main(image, Image.open(fn)).convert('RGB')
-            filename = os.path.join(DIRNAME, '{}_{}.jpg'.format(rand_string(), index))
-            if os.path.exists(filename): os.remove(filename)
+            filename = os.path.join(
+                DIRNAME, '{}_{}.jpg'.format(rand_string(), index))
+            if os.path.exists(filename):
+                os.remove(filename)
             result.save(filename)
             filenames.append(filename)
         return render_template('index.html', filenames=filenames)
     else:
         return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080)
